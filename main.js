66bed3deb452d19590ccd4189fcbc6ea12677bed
ujyr81 <tomek.fortunski@gmail.com>
2020-12-08 17:19:37 +0100

    Responsiveness started
    Responsive menu started, menu button added, offer section heading added

---
+++ main.js
@@ -0,0 +1,11 @@
+const btnMenu = document.querySelector('.header__btn--menu');
+const navMenu = document.querySelector('.header__menu');
+const navList = document.querySelector('.header__list');
+const btnChat = document.querySelector('.header__btn--chat');
+
+btnMenu.addEventListener('click', () => {
+  navMenu.classList.toggle('header__menu--mobile');
+  navList.classList.toggle('header__list--mobile');
+  btnChat.classList.toggle('header__btn--mobile');
+});
+
