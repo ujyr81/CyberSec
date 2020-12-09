ec7a8bca98cdc6062fc852f1c460e9399c12d69d
ujyr81 <tomek.fortunski@gmail.com>
2020-12-09 12:24:14 +0100

    mediaMD query added

--- main.js
+++ main.js
@@ -2,10 +2,17 @@ const btnMenu = document.querySelector('.header__btn--menu');
 const navMenu = document.querySelector('.header__menu');
 const navList = document.querySelector('.header__list');
 const btnChat = document.querySelector('.header__btn--chat');
+const header = document.querySelector('.header');
 
 btnMenu.addEventListener('click', () => {
   navMenu.classList.toggle('header__menu--mobile');
   navList.classList.toggle('header__list--mobile');
   btnChat.classList.toggle('header__btn--mobile');
-  navList.appendChild(btnChat)
+  
+  if(!navList.contains(btnChat)) {
+    navList.appendChild(btnChat)
+  } else {
+    navList.removeChild(btnChat)
+    header.appendChild(btnChat)
+  }
 });
